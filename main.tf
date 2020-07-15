636f22c32c607d7df41c2a954c5f249f4083597e
donbale <christopherbale@icloud.com>
2020-07-15 13:46:41 +0100

    Initial Commit of Project

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "eu-central-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terry-turtle"
+    key    = "default-infrastructure"
+    region = "eu-central-1"
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terry-turtle"
+
+  versioning {
+    enabled = true
+  }
+}
